85410f7ba93f21effd2ec53d943988c375292553
Tangerinez <gtang159@gmail.com>
2019-03-29 16:57:52 -0700

    Transferred JS to external style sheet and going to add media queries - still have some minor bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,63 @@
-/* LOTS of random words */
-var words = ["nasty", "basketball", "immolate", "crib", "competition", "queen", "lawyer", "brash", "flavor", "crack", "top", "drum", "easy", "hat", "bitter", 
+const words = ["nasty", "basketball", "immolate", "crib", "competition", "queen", "lawyer", "brash", "flavor", "crack", "top", "drum", "easy", "hat", "bitter", 
 "filthy", "imbibe", "breezy", "book", "habitual", "compel", "sand", "petite", "boot", "vanish", "earsplitting", "science", "apathetic", "dispensable", "hair",
 "rat", "pump", "renounce", "sort", "earn", "dashing", "resist", "harmony", "invincible", "inspire", "utopian", "astonishing", "macho", "banish", "overconfident",
 "functional", "gusty", "omit", "leather", "interesting", "converse", "bit", "occur", "breakfast", "key", "aboriginal", "terrific", "vigorous", "mailbox", "flee"];
 
-/* Choose random word */
-var randomWord = words[Math.floor(Math.random() * words.length)];
+// Number of guesses left
+let guessesLeft = 8;
+let guessesLeft_div = document.querySelector(".numberGuesses");
 
+// Returns a string
+const randomWord = words[Math.floor(Math.random() * words.length) + 1];
+console.log(randomWord);
 
-let randomWord_div = document.getElementById("randomWord");
+//Each letter of the random word as an index of an array
+const wordChar = randomWord.split('');
 
-/* Creating an array for the answers */
-
-
-
-
-
-var answers = [];
-for (var i = 0; i< randomWord.length; i++) {
+// Returns "_" for every letter in the word
+function blanks() {
+    for (var i = 0; i< randomWord.length; i++) {
     answers[i] = "_";
-}
+    };
+};
 
-var remainingLetters = randomWord.length; /* This value gets -1 every time player guesses a correct letter */
+let wordArray = [];
 
-while (remainingLetters > 0) {      /* Will keep looping until this condition is NOT met */
-    
-}
+// Adds "_" for each letter into an empty array, then joins that array together
+const button = document.querySelector('.start')
+function start(){    
+    //button.addEventListener('click', function() {
+    for (let i = 0; i < randomWord.length; i++) {
+        wordArray.push('_');
+    };
+    button.innerText = wordArray.join(' ');
+    displayLetters();
+};
+//};
+
+
+function displayLetters() {
+const keyboard = document.querySelector('.letters')
+keyboard.onclick = (event) => {      //Every time you click a letter...
+    event.target.style.visibility = 'hidden';   //That element's opacity becomes 0
+    wordChar.forEach((letter, index) => {         
+        if (letter === event.target.innerHTML.toLowerCase()) {    //If letter equals the letter in that index of the array
+            wordArray[index] = letter;          //wordArray at the index of matched letter becomes that letter
+            button.innerText = wordArray.join(' ');          //"_" gets replaced by the new matching letter
+            if (guessesLeft <= 8) {
+                guessesLeft++;    //Adds a +1 first before -1 after clicking the right letter
+            };
+        };
+    });
+    guessWrong();   // Right now this decreases EVERY GUESS BY 1 -> Need to change for only incorrect guesses
+};
+};
+
+function guessWrong() {     //If user guesses letter incorrectly
+    guessesLeft--;
+    guessesLeft_div.textContent = guessesLeft;
+    if (guessesLeft === 0) {
+        setTimeout("alert('You have lost. Please refresh the browser to try again or the game will break.')", 300);
+    };
+};
 
