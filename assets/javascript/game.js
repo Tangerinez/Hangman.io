4d02a52ff30d953bd477ba2ae8c3938196fa2289
Tangerinez <gtang159@gmail.com>
2019-03-29 22:36:13 -0700

    Added media queries, sound for losing and winning, and made an alert for winning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,25 @@ const words = ["nasty", "basketball", "immolate", "crib", "competition", "queen"
 "rat", "pump", "renounce", "sort", "earn", "dashing", "resist", "harmony", "invincible", "inspire", "utopian", "astonishing", "macho", "banish", "overconfident",
 "functional", "gusty", "omit", "leather", "interesting", "converse", "bit", "occur", "breakfast", "key", "aboriginal", "terrific", "vigorous", "mailbox", "flee"];
 
+//Audio Win and Lose
+const audioWin = document.createElement("audio");
+audioWin.setAttribute("src", "assets/images/WinningSong.mp3");
+
+const audioLose = document.createElement("audio");
+audioLose.setAttribute("src", "assets/images/LosingSong.mp3");
+
+//Function for winning and losing song 
+
+function winSong() {
+    let winningSong = document.querySelector("audioYes");
+    audioWin.play();
+}
+
+function loseSong() {
+    let losingSong = document.querySelector("audioNo");
+    audioLose.play();
+}
+
 // Number of guesses left
 let guessesLeft = 8;
 let guessesLeft_div = document.querySelector(".numberGuesses");
@@ -13,6 +32,7 @@ console.log(randomWord);
 
 //Each letter of the random word as an index of an array
 const wordChar = randomWord.split('');
+
 
 // Returns "_" for every letter in the word
 function blanks() {
@@ -44,6 +64,10 @@ keyboard.onclick = (event) => {      //Every time you click a letter...
         if (letter === event.target.innerHTML.toLowerCase()) {    //If letter equals the letter in that index of the array
             wordArray[index] = letter;          //wordArray at the index of matched letter becomes that letter
             button.innerText = wordArray.join(' ');          //"_" gets replaced by the new matching letter
+            if (button.innerText === wordChar.join(" ")) {
+                winSong();
+                setTimeout("alert('Congratulations, you have won! Please refresh the browser to play again or the game will break!')", 300);
+            };
             if (guessesLeft <= 8) {
                 guessesLeft++;    //Adds a +1 first before -1 after clicking the right letter
             };
@@ -57,6 +81,7 @@ function guessWrong() {     //If user guesses letter incorrectly
     guessesLeft--;
     guessesLeft_div.textContent = guessesLeft;
     if (guessesLeft === 0) {
+        loseSong();
         setTimeout("alert('You have lost. Please refresh the browser to try again or the game will break.')", 300);
     };
 };
